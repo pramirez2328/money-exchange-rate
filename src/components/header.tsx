426bb4d931494ea3562d0158e7ab644986af7b8c
Pedro Ramirez <prlara2000@gmail.com>
2024-05-13 23:10:48 -0500

    add border

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,8 +3,11 @@ function header() {
   const today = date.toISOString().split('T')[0];
 
   return (
-    <div className='flex justify-between w-full bg-slate-200 p-6 text-gray-500'>
-      <h1 className='text-gray-500 text-3xl uppercase tracking-wider'>Exchange Rate</h1>
+    <div className='flex justify-between w-full bg-slate-100 p-6 text-gray-500'>
+      <div className='shadow-2xl p-2 rounded-md'>
+        <h1 className='text-gray-500 text-3xl uppercase tracking-wider '>Exchange Rate</h1>
+      </div>
+
       <h3>{today}</h3>
     </div>
   );

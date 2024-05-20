a9e6550379623d78730566bd0c9261c522a701bb
Pedro Ramirez <prlara2000@gmail.com>
2024-05-20 18:52:19 -0500

    add input

--- src/components/Converter.tsx
+++ src/components/Converter.tsx
@@ -1,9 +1,11 @@
 function Converter() {
   return (
-    <div className='container w-9/12 rounded-lg mt-12 flex flex-col justify-center '>
-      <h1 className='border-2 shadow-2xl mb-4 text-center text-4xl'>CONVERSE RATE</h1>
+    <div className='container  w-11/12 md:w-9/12 rounded-lg mt-12 flex flex-col justify-center '>
+      <h1 className='border-2 shadow-2xl mb-4 text-center text-4xl p-4 rounded mb-8'>CONVERSION RATE</h1>
       <div className='flex w-full justify-between'>
-        <div className='w-3/12'>
+        <div className='w-5/12 flex border border-stone-500 p-2 rounded'>
+          <input type='number' className='w-6/12' />
+          <span className='mx-2'>|</span>
           <select name='from-currency' id='from' className='w-full'>
             <option value='USD'>USD</option>
             <option value='EUR'>EUR</option>
@@ -13,7 +15,9 @@ function Converter() {
           </select>
         </div>
 
-        <div className='w-3/12'>
+        <div className='w-5/12 flex border border-stone-500 p-2 rounded'>
+          <input type='number' className='w-6/12' />
+          <span className='mx-2'>|</span>
           <select name='to-currency' id='to' className='w-full'>
             <option value='USD'>USD</option>
             <option value='EUR'>EUR</option>

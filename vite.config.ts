c3ce757e4af374ba43d53395dfcf5bb2bf22ebbf
Pedro Ramirez <prlara2000@gmail.com>
2024-05-29 23:15:03 -0500

    add cli

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/', // Set this to your specific base URL if necessary
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist', // Ensure this matches your Amplify build settings
+    sourcemap: true, // Optional: Useful for debugging
+  },
+  define: {
+    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+  },
+});
